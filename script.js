cf9bbeb12bdd411b1fa2db9a63cb7129bda838a8
Joshua <gallopingbull89@gmail.com>
2022-10-22 17:44:34 -0700

    added template banner projects

--- script.js
+++ script.js
@@ -18,38 +18,40 @@ searchInput.addEventListener("input", e => {
 })
 
 
-  $(document).ready(function() {  
+$(document).ready(function () {
 
   fetch("./data/projectsData.json")
-  .then(res => res.json())
-  .then(data => {
-    users = data.map(user =>	 {
-      const card = userCardTemplate.content.cloneNode(true).children[0]
-   
-      const header = card.querySelector("[data-header]")
-      const body = card.querySelector("[data-body]")
-      const tools = card.querySelector("[data-tools]")
-      const path = card.querySelector("[data-path]")
-      const image = card.querySelector("[data-image]")
+    .then(res => res.json())
+    .then(data => {
+      users = data.map(user => {
+        const card = userCardTemplate.content.cloneNode(true).children[0]
 
-      header.textContent = user.gametitle
-      body.textContent = user.role
-      tools.textContent = user.tools  
-      path.href = user.url//"projects/" + usur.url + /"index.HTML"
-      //image.src = user.bannerimage//"projects/" + user.bannerImage + /"banner.png"
+        const header = card.querySelector("[data-header]")
+        const body = card.querySelector("[data-body]")
+        const tools = card.querySelector("[data-tools]")
+        const path = card.querySelector("[data-path]")
+        const image = card.querySelector("[data-image]")
 
-      console.log(path)
-      console.log(user.url)
-  
-  
-      userCardContainer.append(card)
-      return { gametitle: user.gametitle, role: user.role, tools: user.tools, path: user.path, element: card }
-    }
-    
-    )
-	
-    //I might delete this line... maybe should
-    //.catch(error => console.log(error));
-  })
+        header.textContent = user.gametitle
+        body.textContent = user.role
+        tools.textContent = user.tools
+        path.href = user.url//"projects/" + usur.url + /"index.HTML"
+        image.src = user.bannerImage//"projects/" + user.bannerImage + /"banner.png"
 
-  }); 
+        console.log(path)
+        console.log(user.bannerImage)
+        console.log(user.url)
+
+
+        userCardContainer.append(card)
+        return { gametitle: user.gametitle, role: user.role, tools: user.tools, path: user.path, element: card }
+      }
+
+      )
+
+      //I might delete this line... maybe should
+      //.catch(error => console.log(error));
+    })
+
+});
+
